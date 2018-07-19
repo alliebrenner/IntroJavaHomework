64d8322671dded35e183a5478aac793582da5c87
Allie Brenner <abrenner@oxy.edu>
2018-07-19 12:26:24 -0700

    Assignment 3 Commit

---
+++ AssignmentThreeClasses/src/Employee.java
@@ -0,0 +1,22 @@
+
+public class Employee {
+private String employee_name;
+private double employee_salary;
+
+public Employee(String employeeName, double currentSalary){
+	employee_name=employeeName;
+	employee_salary=currentSalary;
+}
+public String getName(){
+	return employee_name;
+}
+public double getSalary(){
+	return employee_salary;
+}
+public void raiseSalary(double percentIncrease){
+	employee_salary = employee_salary + (employee_salary *percentIncrease/100);
+	System.out.println("Percent Increase "+ percentIncrease+ "%");
+}
+
+
+}
